7c6787d8bd838f07e53494753fdaebfe74da4a8f
Jeremy Kennedy <me@jeremyk.net>
2021-11-11 20:29:03 -0500

    clean up and polish logging

--- ExampleHttpFunction/index.ts
+++ ExampleHttpFunction/index.ts
@@ -1,17 +1,16 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully!!"
-        : "This HTTP triggered function executed successfully!! Pass a name in the query string or in the request body for a personalized response.";
-    context.bindings.outputBlob = req
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log('HTTP trigger function processed a request.');
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = `Hello, ${name}. This HTTP triggered function executed successfully!`;
+  context.bindings.outputBlob = JSON.stringify(req, null, 2);
+  context.res = {
+    body: responseMessage,
+  };
 };
 
 export default httpTrigger;
